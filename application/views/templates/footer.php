b31783b65dfde1f5a0b3237a15031012a770f397
aldihagi <rubenhagi21@gmail.com>
2021-12-04 15:30:49 +0700

    DONE

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Web Programming Univ. BSI with Bootstrap SB Admin 2 <?= date('Y'); ?></span>
+            <span>Copyright &copy; Web Programming Univ. BSI By; 19201106_Hagi Geraldi <?= date('Y'); ?></span>
         </div>
     </div>
 </footer>
